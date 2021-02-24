fb8263135ec29c45b51f3646f683c7b3b0920004
Antoine Muller <niantoinesan@gmail.com>
2021-02-24 10:10:01 +0100

    add votes sql migration

---
+++ prisma/migrations/20210224083946_create_votes/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "votes" (
+    "id" SERIAL NOT NULL,
+    "question_id" UUID NOT NULL,
+    "predefined_answer_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "votes" ADD FOREIGN KEY ("question_id") REFERENCES "questions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "votes" ADD FOREIGN KEY ("predefined_answer_id") REFERENCES "predefined_answers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- Listen/notify new votes
+
+CREATE OR REPLACE FUNCTION notify_new_vote()
+RETURNS trigger AS $$
+BEGIN
+    PERFORM pg_notify(
+    'new_vote',
+    row_to_json(NEW)::text
+  );
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "after_vote" 
+    AFTER INSERT ON "votes"
+    FOR EACH ROW
+    EXECUTE PROCEDURE notify_new_vote()
