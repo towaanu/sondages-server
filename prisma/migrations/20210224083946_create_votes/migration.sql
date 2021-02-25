aa5fd421e3b6ba9f645442abc90a66f33f66be2e
Antoine Muller <niantoinesan@gmail.com>
2021-02-25 08:57:49 +0100

    remove unused trigger

--- prisma/migrations/20210224083946_create_votes/migration.sql
+++ prisma/migrations/20210224083946_create_votes/migration.sql
@@ -10,21 +10,3 @@ CREATE TABLE "votes" (
 
 -- AddForeignKey
 ALTER TABLE "votes" ADD FOREIGN KEY ("predefined_answer_id") REFERENCES "predefined_answers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- Listen/notify new votes
-
-CREATE OR REPLACE FUNCTION notify_new_vote()
-RETURNS trigger AS $$
-BEGIN
-    PERFORM pg_notify(
-    'new_vote',
-    row_to_json(NEW)::text
-  );
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER "after_vote" 
-    AFTER INSERT ON "votes"
-    FOR EACH ROW
-    EXECUTE PROCEDURE notify_new_vote();
